d5fcaa46b18b8551014e961355cd8de46a68db92
Augusto Rucle <augustorucle@gmail.com>
2019-01-22 19:09:36 -0600

    Se agrego repositorio de usuarios

---
+++ spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.springframework.samples.petclinic.user;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.samples.petclinic.user.User;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ *
+ * @author AugustoRuCle
+ */
+public interface UserRepository extends Repository<User, Integer>{
+    /**
+     * Save an {@link User} to the data store, either inserting or updating it.
+     * @param user the {@link Owner} to save
+    */
+    void save(User user);
+}
