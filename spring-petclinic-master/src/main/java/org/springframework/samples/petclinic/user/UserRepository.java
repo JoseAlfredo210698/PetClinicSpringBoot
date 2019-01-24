aa87020f11555fa16c484947bfc74f277b5e3e0d
Augusto Rucle <augustorucle@gmail.com>
2019-01-23 22:23:30 -0600

    Agrego busqueda de email

--- spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
+++ spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
@@ -6,7 +6,6 @@
 package org.springframework.samples.petclinic.user;
 
 import java.util.Collection;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.samples.petclinic.user.User;
@@ -19,9 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
  * @author AugustoRuCle
  */
 public interface UserRepository extends Repository<User, Integer>{
+    
+    @Query("SELECT user FROM User user WHERE user.email = :email")
+    @Transactional(readOnly = true)
+    User findByEmail(@Param("email") String email);
+    
     /**
      * Save an {@link User} to the data store, either inserting or updating it.
      * @param user the {@link Owner} to save
     */
     void save(User user);
+   
 }
