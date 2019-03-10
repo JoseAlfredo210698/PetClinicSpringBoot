4f9114774ab45af3bd6e975b8e2b79b30fd50e48
JoseAlfredo210698 <163204@ids.upchiapas.edu.mx>
2019-03-09 20:12:06 -0600

    Controllers para citas

---
+++ spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/appointment/Specialties.java
@@ -0,0 +1,33 @@
+
+package org.springframework.samples.petclinic.appointment;
+
+
+import java.sql.Date;
+import java.sql.Time;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import org.springframework.samples.petclinic.model.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+
+@Entity
+@Table(name = "specialties")
+
+public class Specialties extends BaseEntity{
+    @Column(name = "name")
+    @NotEmpty
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
